e344e3fa21b0ea885b114828364c8c1e4800ca93
domesya <163451587+domesya@users.noreply.github.com>
2026-09-08 19:51:50 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,17 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q helix | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/256x256/apps/helix.png
+export DESKTOP=/usr/share/applications/Helix.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
+quick-sharun \
+  /usr/bin/helix \
+  /usr/lib/helix
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
