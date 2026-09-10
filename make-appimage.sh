e90fbc1f145c586c891a451d15c264bafff5f605
domesya <163451587+domesya@users.noreply.github.com>
2026-09-10 11:14:35 +0200

    Add URUNTIME_PRELOAD to make-appimage.sh
    Add URUNTIME_PRELOAD environment variable for AppImage.

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/helix.png
 export DESKTOP=/usr/share/applications/Helix.desktop
+export URUNTIME_PRELOAD=1
 export STARTUPWMCLASS=helix
 
 
